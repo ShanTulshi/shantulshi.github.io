da7f34c103f28a2913091c5b86391a439edddfa0
Shan Tulshi <shan.tulshi@gmail.com>
2016-09-13 17:49:22 -0500

    fixed the last mistake

--- files/document_test.c
+++ files/document_test.c
@@ -4,5 +4,49 @@
  */
 
 #include "document.h"
+#include <stdio.h>
+#include <string.h>
+
 // test your document here
-int main() { return 0; }
+void * cctor(void * elem) {
+	return strdup((char *)elem);
+}
+
+void dtor (void * elem) {
+	free(elem);
+}
+
+int main(int argc, char ** argv) {
+	int c = 0;
+	char ** files = NULL;
+	if(argc > 0) {
+		files = (char **) malloc((argc - 1) * sizeof(char *));
+		int i = 1;
+		for(; i < argc; i++) files[i - 1] = argv[i];
+		c = argc;
+	}
+	else {
+		files = (char **) malloc(sizeof(char *));
+		files[0] = "test.txt";
+		c = 1;
+	}
+	size_t out_file_name_len = 30;
+	char * out_file_name = malloc(out_file_name_len);
+	int i = 0;
+	for(i = 0; i < c; i++) {
+		printf("=====DOCUMENT #%d=====\n", i+1);
+		Document * d = Document_create_from_file(files[i]);
+		printf("document size: %lu\n", Document_size(d));
+		printf("document printed below: \n");
+		size_t j = 1;
+		for(j = 1; j <= Document_size(d); j++)
+			printf("%s\n", Document_get_line(d, j));
+
+		printf("Writing to file: \'output%d.txt\'\n", i+1);
+		snprintf(out_file_name, out_file_name_len, "output%d.txt", i+1);
+		Document_write_to_file(d, out_file_name);
+		Document_destroy(d);
+		printf("\n\n");
+	}
+	return 0;
+}
