24ed9d3618bc18dff3e6f3ba20013539e945ff60
Shan Tulshi <shan.tulshi@gmail.com>
2018-08-18 22:57:03 -0700

    Resume added\!

--- src/App.tsx
+++ src/App.tsx
@@ -14,13 +14,15 @@ import moi from './images/avatar.jpg';
 class App extends React.Component {
   public render() {
     return (
-      <div className="Main">
-        <header className="Main-header">
-          <img src={moi} className="Main-portrait" alt="Portrait" />
-          <h1 className="Main-title">Shantanu Tulshibagwale</h1>
-          <RotateText />
-          <Links />
-        </header>
+      <div>
+        <div className="Main">
+          <header className="Main-header">
+            <img src={moi} className="Main-portrait" alt="Portrait" />
+            <h1 className="Main-title">Shantanu Tulshibagwale</h1>
+            <RotateText />
+            <Links />
+          </header>
+        </div>
       </div>
     );
   }
