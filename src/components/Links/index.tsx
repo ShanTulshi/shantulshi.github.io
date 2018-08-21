ae6fc50d51463ad57918a204ee135cbed886d642
Shan Tulshi <shan.tulshi@gmail.com>
2018-08-21 00:01:16 -0700

    Added web resume link, added title-text to all links

--- src/components/Links/index.tsx
+++ src/components/Links/index.tsx
@@ -8,11 +8,12 @@ class Links extends Component<{}, {}> {
 	public render(): React.ReactNode {
 		return (
 			<ul className="icons">
-				<li><a target="_blank" href="https://github.com/ShanTulshi" className="icon-github-circled"/></li>
-				<li><a target="_blank" href="https://instagram.com/shantulshi" className="icon-instagram"/></li>
-				<li><a target="_blank" href="https://linkedin.com/in/shantulshi" className="icon-linkedin"/></li>
-				<li><a target="_blank" href="resume.pdf" className="icon-doc-text"/></li>
-				<li><a target="_blank" href="mailto:shan@notgood.site?body=%0A%0A-Sent%20from%20website." className="icon-mail"/></li>
+				<li><a target="_blank" title="GitHub" href="https://github.com/ShanTulshi" className="icon-github-circled"/></li>
+				<li><a target="_blank" title="Instagram" href="https://instagram.com/shantulshi" className="icon-instagram"/></li>
+				<li><a target="_blank" title="LinkedIn" href="https://linkedin.com/in/shantulshi" className="icon-linkedin"/></li>
+				<li><a target="_blank" title="PDF Resume" href="resume.pdf" className="icon-doc-text"/></li>
+				<li><a target="_blank" title="Web Resume" href="resume" className="icon-file-code"/></li>
+				<li><a target="_blank" title="Contact" href="mailto:shan@notgood.site?body=%0A%0A-Sent%20from%20website." className="icon-mail"/></li>
 			</ul>
 		)
 	}
